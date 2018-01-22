73838364d6f3dc0152e3cf37c2259106d647628d
unknown <sampv67@gmail.com>
2018-01-22 21:40:50 +0000

    more composerthings

--- index.php
+++ index.php
@@ -1,7 +1,18 @@
-<!DOCTYPE HTML>
-<html>
-<body>
+<?php
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
 
-<?php echo "THIS IS A TEST DONATION" ?>
-</body>
-</html>
+require './vendor/autoload.php';
+
+$app = new \Slim\App;
+
+$app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
+    $name = $args['name'];
+    $response->getBody()->write("Hello, $name");
+
+    return $response;
+});
+$app->run();
+
+
+?>
